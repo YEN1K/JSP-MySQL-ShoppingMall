a45a0c5ccc60354e7cb3f9bd1fecfc1518597eca
YEN1K <k4368284@naver.com>
2026-01-28 22:21:39 +0900

    db: 상품 테이블(item_tbl) 생성

---
+++ sql/item_tbl.sql
@@ -0,0 +1,10 @@
+CREATE TABLE item_tbl(
+   item_no INT PRIMARY KEY,
+   item_category VARCHAR(20),
+   item_name VARCHAR(50),
+   item_price INT,
+   item_image VARCHAR(30),
+   item_info VARCHAR(50),
+   item_discount INT,
+   item_soldCount INT
+);
